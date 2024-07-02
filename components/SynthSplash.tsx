d89f9295f5ce64f54f2f848bed2d08c8f58b4417
capo_greco <thomas.capogreco@gmail.com>
2024-07-02 22:21:08 +1000

    SynthSplash ✅

---
+++ components/SynthSplash.tsx
@@ -0,0 +1,17 @@
+export function SynthSplash () {
+   return (
+      <div style="
+         font: italic bolder 80px sans-serif;
+         color: white;
+         display: flex;
+         justify-content: center;
+         align-items: center;
+         position: fixed;
+         width: 100vw;
+         height: 100vh;
+         left: 0;
+         top: 0;"
+      > → ENABLE </div>
+   );
+ }
+ 
